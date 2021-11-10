a9bc28d448ae3d2e7e30118f5b03a3e16126bd56
Wiku <Wiku@WikuPC>
2021-11-10 22:48:50 +0100

    >Añadido panel de Scroll en juegos >Ordenado panel de scroll de juevos >Añadidas imagenes de archivos >Cambios menores

--- src/proyectodisp/PanelJuegos.java
+++ src/proyectodisp/PanelJuegos.java
@@ -27,36 +27,255 @@ public class PanelJuegos extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jScrollPane1.getVerticalScrollBar().setUnitIncrement(40);
+        jPanel5 = new javax.swing.JPanel();
+        imgMario64 = new javax.swing.JLabel();
+        imgMinecraftJava = new javax.swing.JLabel();
+        imgMarioOddisey = new javax.swing.JLabel();
+        imgCeleste = new javax.swing.JLabel();
+        imgMarioKartDeluxe = new javax.swing.JLabel();
+        imgPortal = new javax.swing.JLabel();
+        imgSuperMetroid = new javax.swing.JLabel();
+        imgMarioSunshine = new javax.swing.JLabel();
+        imgSeterra = new javax.swing.JLabel();
+        imgDiversion = new javax.swing.JLabel();
+        imgRobloSpeedrun4 = new javax.swing.JLabel();
+        imgSuperMarioWorld = new javax.swing.JLabel();
+        imgZelda64 = new javax.swing.JLabel();
+        imgSuperMario = new javax.swing.JLabel();
+        imgZeldaALTTP = new javax.swing.JLabel();
+        imgResidentEvil2k19 = new javax.swing.JLabel();
+        imgSBSPBBP = new javax.swing.JLabel();
+        imgMinecraftMalo = new javax.swing.JLabel();
+        imgRefunct = new javax.swing.JLabel();
+        imgOutlast = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
-        jPanel1.setBackground(new java.awt.Color(102, 255, 102));
+        jScrollPane1.setBackground(new java.awt.Color(0, 0, 0));
+        jScrollPane1.setBorder(null);
+        jScrollPane1.setForeground(new java.awt.Color(0, 0, 0));
+        jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        jScrollPane1.setPreferredSize(new java.awt.Dimension(1280, 660));
+        jScrollPane1.addMouseWheelListener(new java.awt.event.MouseWheelListener() {
+            public void mouseWheelMoved(java.awt.event.MouseWheelEvent evt) {
+                jScrollPane1MouseWheelMoved(evt);
+            }
+        });
+        jScrollPane1.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentShown(java.awt.event.ComponentEvent evt) {
+                jScrollPane1ComponentShown(evt);
+            }
+        });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1280, Short.MAX_VALUE)
+        jPanel5.setBackground(new java.awt.Color(51, 51, 51));
+        jPanel5.setPreferredSize(new java.awt.Dimension(1264, 2131));
+
+        imgMario64.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/mario.png"))); // NOI18N
+
+        imgMinecraftJava.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/minecraft.png"))); // NOI18N
+
+        imgMarioOddisey.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/marioodisi.png"))); // NOI18N
+
+        imgCeleste.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/celeste.png"))); // NOI18N
+
+        imgMarioKartDeluxe.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/mariokart8deluxe.png"))); // NOI18N
+
+        imgPortal.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/portal.png"))); // NOI18N
+
+        imgSuperMetroid.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/supermetrod.png"))); // NOI18N
+
+        imgMarioSunshine.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/mariosunshine.png"))); // NOI18N
+
+        imgSeterra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/seterra.png"))); // NOI18N
+
+        imgDiversion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/eljuegomasdivertidodelahistoria.png"))); // NOI18N
+
+        imgRobloSpeedrun4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/robloxspeedrun4.png"))); // NOI18N
+
+        imgSuperMarioWorld.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/supermarioworld.png"))); // NOI18N
+
+        imgZelda64.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/zelda64.png"))); // NOI18N
+
+        imgSuperMario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/supermario.png"))); // NOI18N
+
+        imgZeldaALTTP.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/zeldaalinktothepast.png"))); // NOI18N
+
+        imgResidentEvil2k19.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/residentevil22019.png"))); // NOI18N
+
+        imgSBSPBBP.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/sbspbfbb.png"))); // NOI18N
+
+        imgMinecraftMalo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/minecraftwin10.png"))); // NOI18N
+
+        imgRefunct.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/refunct.png"))); // NOI18N
+
+        imgOutlast.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectodisp/images/outlast.png"))); // NOI18N
+
+        jLabel1.setFont(new java.awt.Font("Ebrima", 1, 14)); // NOI18N
+        jLabel1.setText("Super Mario 64");
+
+        jLabel2.setText("Minecraft: Java Edition");
+
+        jLabel3.setText("Super Mario Odyssey");
+
+        jLabel4.setText("Celeste");
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel5Layout.createSequentialGroup()
+                .addGap(48, 48, 48)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel5Layout.createSequentialGroup()
+                        .addComponent(imgZelda64)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel5Layout.createSequentialGroup()
+                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(imgSeterra)
+                            .addGroup(jPanel5Layout.createSequentialGroup()
+                                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(imgMario64, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(imgMarioKartDeluxe, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(imgSBSPBBP))
+                                .addGap(48, 48, 48)
+                                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(imgMinecraftJava)
+                                    .addComponent(imgPortal)
+                                    .addComponent(imgDiversion)
+                                    .addComponent(imgSuperMario)
+                                    .addComponent(imgMinecraftMalo))
+                                .addGap(48, 48, 48)
+                                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addGroup(jPanel5Layout.createSequentialGroup()
+                                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(imgSuperMetroid)
+                                            .addComponent(imgMarioOddisey))
+                                        .addGap(48, 48, 48)
+                                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(imgCeleste)
+                                            .addComponent(imgMarioSunshine)))
+                                    .addGroup(jPanel5Layout.createSequentialGroup()
+                                        .addComponent(imgSuperMarioWorld)
+                                        .addGap(48, 48, 48)
+                                        .addComponent(imgRobloSpeedrun4))
+                                    .addGroup(jPanel5Layout.createSequentialGroup()
+                                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(imgZeldaALTTP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                            .addComponent(imgRefunct, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(imgResidentEvil2k19, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                            .addComponent(imgOutlast, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))))
+                        .addGap(0, 48, Short.MAX_VALUE))))
+            .addGroup(jPanel5Layout.createSequentialGroup()
+                .addGap(123, 123, 123)
+                .addComponent(jLabel1)
+                .addGap(197, 197, 197)
+                .addComponent(jLabel2)
+                .addGap(192, 192, 192)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel4)
+                .addGap(151, 151, 151))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 660, Short.MAX_VALUE)
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel5Layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(imgMario64)
+                    .addComponent(imgMinecraftJava)
+                    .addComponent(imgMarioOddisey)
+                    .addComponent(imgCeleste))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGap(33, 33, 33)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(imgMarioKartDeluxe)
+                    .addComponent(imgSuperMetroid)
+                    .addComponent(imgPortal)
+                    .addComponent(imgMarioSunshine))
+                .addGap(67, 67, 67)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(imgRobloSpeedrun4)
+                    .addComponent(imgDiversion)
+                    .addComponent(imgSeterra)
+                    .addComponent(imgSuperMarioWorld))
+                .addGap(67, 67, 67)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(imgSuperMario)
+                    .addComponent(imgZelda64)
+                    .addComponent(imgZeldaALTTP)
+                    .addComponent(imgResidentEvil2k19))
+                .addGap(67, 67, 67)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(imgRefunct, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(imgMinecraftMalo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(imgSBSPBBP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(imgOutlast, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(49, Short.MAX_VALUE))
         );
+
+        jScrollPane1.setViewportView(jPanel5);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jScrollPane1MouseWheelMoved(java.awt.event.MouseWheelEvent evt) {//GEN-FIRST:event_jScrollPane1MouseWheelMoved
+    
+    }//GEN-LAST:event_jScrollPane1MouseWheelMoved
+
+    private void jScrollPane1ComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jScrollPane1ComponentShown
+          
+    }//GEN-LAST:event_jScrollPane1ComponentShown
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel imgCeleste;
+    private javax.swing.JLabel imgDiversion;
+    private javax.swing.JLabel imgMario64;
+    private javax.swing.JLabel imgMarioKartDeluxe;
+    private javax.swing.JLabel imgMarioOddisey;
+    private javax.swing.JLabel imgMarioSunshine;
+    private javax.swing.JLabel imgMinecraftJava;
+    private javax.swing.JLabel imgMinecraftMalo;
+    private javax.swing.JLabel imgOutlast;
+    private javax.swing.JLabel imgPortal;
+    private javax.swing.JLabel imgRefunct;
+    private javax.swing.JLabel imgResidentEvil2k19;
+    private javax.swing.JLabel imgRobloSpeedrun4;
+    private javax.swing.JLabel imgSBSPBBP;
+    private javax.swing.JLabel imgSeterra;
+    private javax.swing.JLabel imgSuperMario;
+    private javax.swing.JLabel imgSuperMarioWorld;
+    private javax.swing.JLabel imgSuperMetroid;
+    private javax.swing.JLabel imgZelda64;
+    private javax.swing.JLabel imgZeldaALTTP;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
 
